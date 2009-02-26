8412c46d77bcce169cfe47fb338e2a728ef3e094
dansantos User <dansantos@ubuntu.ubuntu-domain>
2009-02-26 09:09:31 -0300

    segunda versão

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -52,7 +52,6 @@ class UsersController < ApplicationController
   # GET /users/new.xml
   def new
     @user = User.new
-
     respond_to do |format|
       format.html # new.html.erb
       format.xml  { render :xml => @user }
