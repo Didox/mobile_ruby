228504b4c59bc5e64806f41371cd5dedecdcb7ba
dansantos User <dansantos@ubuntu.ubuntu-domain>
2009-03-02 17:21:24 -0300

    validator

--- app/models/full_banner.rb
+++ app/models/full_banner.rb
@@ -1,4 +1,13 @@
 class FullBanner < ActiveRecord::Base
+  validates_presence_of  :name, :file, :link, :date_begin, :date_end
+  #validates_numericality_of :price
+  #validate :upload_file
+  #validates_uniqueness_of :title
+  validates_format_of :file,
+                    :with    => %r{\.(gif|jpg|png)$}i,
+                    :message => 'Aceito somente os formatos (gif, jpg, pgn)'
+
+
   has_and_belongs_to_many :pages
   before_update :upload_file
   after_create :upload_file_save
